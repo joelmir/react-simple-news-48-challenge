9d20b61a791e43e3252ad44596834854edf75a5b
Joelmir Ribacki <joedekid@gmail.com>
2018-10-22 18:41:13 -0300

    added bar and menus to redirect to home and contact page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { HashRouter, Route } from "react-router-dom"
 import Home from './views/Home'
 import Contact from './views/Contact'
+import AppBar from './components/appBar/AppBar'
 
 const App = () => {
     return(
         <HashRouter>
             <div>
+                <AppBar/>
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/contact" component={Contact}/>
             </div>
---
+++ src/components/appBar/AppBar.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+import HomeIcon from '@material-ui/icons/Home';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { Link } from 'react-router-dom'
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+});
+
+function SearchAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} component={Link} to='/'  color="inherit" aria-label="Open drawer">
+            <HomeIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+            This is a simple react news app
+          </Typography>
+          <div className={classes.grow} />
+          <IconButton className={classes.menuButton} component={Link} to='/contact' color="inherit" aria-label="Open drawer">
+            <AccountCircleIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+SearchAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SearchAppBar);
